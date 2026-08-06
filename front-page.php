c4fe9c1222be7530e63ce3751282cae564da04f5
Mateo521 <66658355+Mateo521@users.noreply.github.com>
2026-08-06 09:29:49 -0300

    fixs

--- front-page.php
+++ front-page.php
@@ -199,7 +199,7 @@ get_header();
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
             <p class="font-ui text-sm">
-                Información actualizada a <strong><?php echo get_the_modified_date('F \d\e Y'); ?></strong>.
+                Información actualizada a <strong><?php echo esc_html( get_the_modified_date( 'F \d\e Y', get_queried_object_id() ) ); ?></strong>.
             </p>
         </div>
     </div>
